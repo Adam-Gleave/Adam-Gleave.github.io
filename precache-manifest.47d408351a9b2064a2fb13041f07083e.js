fd8bd633a344058db9a22857962e04c28165c88f
Adam-Gleave <adamg108@hotmail.co.uk>
2020-03-30 02:46:38 +0100

    Updates

---
+++ precache-manifest.47d408351a9b2064a2fb13041f07083e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95464d6f1507031c6e5e7771cbd26bf5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "09df669d6837717dadf3",
+    "url": "/portfolio/static/css/main.0926c7eb.chunk.css"
+  },
+  {
+    "revision": "6f7ef14e9a7884f125dc",
+    "url": "/portfolio/static/js/2.eb244040.chunk.js"
+  },
+  {
+    "revision": "b803c26b5bb963d4a54b0b1b419ee973",
+    "url": "/portfolio/static/js/2.eb244040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09df669d6837717dadf3",
+    "url": "/portfolio/static/js/main.fec74dc4.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
