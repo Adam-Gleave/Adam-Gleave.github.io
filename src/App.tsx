380a4d01dfafc976745c4aed9bfbdd3d9ec121d5
Adam-Gleave <adamg108@hotmail.co.uk>
2020-04-01 19:43:51 +0100

    Add flip animation when clicking a project card

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import * as THREE from 'three';
-import React, { CSSProperties, useRef, useEffect } from 'react';
+import React, { CSSProperties, useRef, useEffect, useState } from 'react';
 import { Canvas, useFrame, useThree } from 'react-three-fiber';
-import { useSpring, animated } from 'react-spring';
+import { animated, interpolate, useSpring } from 'react-spring';
+import { Flipper, Flipped } from 'react-flip-toolkit';
 import { makeNoise2D } from 'open-simplex-noise';
 
 import './App.css';
@@ -76,52 +77,87 @@ function Terrain() {
   );
 }
 
-const trans: any = (s: any) => `scale(${s})`;
-
-type CardProps = {
+type CardFrontProps = {
   headerText: string,
   style: CSSProperties,
+  toggleFlipped: () => void,
 };
 
-function cardProps(headerText: string, imageUrl: string): CardProps {
+function makeProps(headerText: string, imageUrl: string, toggleFlipped: () => void): CardFrontProps {
   let style: CSSProperties = { backgroundImage: `url(${imageUrl})` };
   
   return {
     headerText,
-    style
+    style,
+    toggleFlipped,
   };
 }
 
+const CardFront = (props: CardFrontProps) => (
+  <Flipped flipId="card">
+    <div className="card" onClick={props.toggleFlipped}>
+      <div className="card-header" style={{display: "absolute"}}>
+        {props.headerText}
+        <hr className="card-header-line" />
+        <div className="card-content" style={props.style} />
+      </div>
+    </div>
+  </Flipped>
+);
+
+type CardBackProps = {
+  headerText: string,
+  toggleFlipped: () => void,
+};
+
+const CardBack = (props: CardBackProps) => (
+  <Flipped flipId="card">
+    <div className="card" onClick={props.toggleFlipped}>
+      <div className="card-header" style={{display: "absolute"}}>
+        {props.headerText}
+        <hr className="card-header-line" />
+        <div className="card-content" />
+      </div>
+    </div>
+  </Flipped>
+);
+
+type CardProps = {
+  headerText: string,
+  url: string
+};
+
 const Card = (cardProps: CardProps) => {
-  const cardRef = useRef<any>(null);
+  const [flipped, setFlipped] = useState(false);
+  const [scale, setScale] = useState(1);
 
-  const [props, set] = useSpring(() => ({ 
-    s: 1, 
-    config: { mass: 1, tension: 100, friction: 10 } 
-  }));
+  const toggleFlipped = () => setFlipped(flipped => !flipped);
 
-  function style(): React.CSSProperties {
-    return {
-      display: 'absolute',
-      transform: props.s.interpolate(trans),
-    };
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg) scale(${scale})`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
+
+  function opacityFunc(o: any): number {
+    if (typeof o === "number") {
+      return 1 - o;
+    }
+    return 1;
   }
 
   return (
-    <>
-      <animated.div
-      ref={cardRef}
-      className="card"
-      style={style()}
-      onMouseMove={() => set ({ s: 1.1 })}
-      onMouseLeave={() => set({ s: 1 })}>
-        <div className="card-header">
-          {cardProps.headerText}
-          <hr className="card-header-line"></hr>
-          <div className="card-content" style={cardProps.style}></div>
-        </div>
-      </animated.div>
-    </>
+    <Flipper flipKey={flipped} spring="gentle">
+      {flipped ? (
+        <animated.div style={{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`) }}>
+          <CardBack headerText={cardProps.headerText} toggleFlipped={toggleFlipped} />
+        </animated.div>
+      ) : (
+        <animated.div onMouseEnter={() => setScale(1.1)} onMouseLeave={() => setScale(1)} style={{ opacity: opacity.interpolate(o => opacityFunc(o)), transform }}>
+          <CardFront {...makeProps(cardProps.headerText, cardProps.url, toggleFlipped)} />
+        </animated.div>
+      )}
+    </Flipper>
   );
 }
 
@@ -136,22 +172,22 @@ function App() {
       </Canvas>
       <div className="grid-container">
         <div className="grid">
-          <Card {...cardProps("OpenMW", "https://i.ytimg.com/vi/izlm2CAnCpY/maxresdefault.jpg")} />
-          <Card {...cardProps("OpenCK", "https://imgur.com/Zfh3eDn.png")} />
-          <Card {...cardProps("Rustbucket", "https://i.imgur.com/xCYk9h7.png")} />
-          <Card {...cardProps("Voxels", "https://imgur.com/ucOoqnc.png")} />
+          <Card headerText="OpenMW" url="https://i.ytimg.com/vi/izlm2CAnCpY/maxresdefault.jpg" />
+          <Card headerText="OpenCK" url="https://imgur.com/Zfh3eDn.png" />
+          <Card headerText="Rustbucket" url="https://i.imgur.com/xCYk9h7.png" />
+          <Card headerText="Voxels" url="https://imgur.com/ucOoqnc.png" />
         </div>
       </div>
       <div>
         <div className="footer-container">
           <div className="footer">
-            <a href="https://github.com/Adam-Gleave/" target="_blank">
+            <a href="https://github.com/Adam-Gleave/" target="_blank" draggable={false}>
               <img src={process.env.PUBLIC_URL + "/images/GitHub-Mark-Light-32px.png"} draggable={false} />
             </a>
-            <a href="mailto:adamgleave97@gmail.com" >
+            <a href="mailto:adamgleave97@gmail.com" draggable={false}>
               <img src={process.env.PUBLIC_URL + "/images/mail-32.png"} draggable={false} />
             </a>
-            <a href="https://www.linkedin.com/in/adam-gleave/" target="_blank">
+            <a href="https://www.linkedin.com/in/adam-gleave/" target="_blank" draggable={false}>
               <img src={process.env.PUBLIC_URL + "/images/linkedin-5-32.png"} draggable={false} />
             </a>
           </div>
